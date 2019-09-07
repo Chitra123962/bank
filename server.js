9df5df77337c0ccde61ca3b1449e1401f38e3ca5
Chitra123962 <55015961+Chitra123962@users.noreply.github.com>
2019-09-07 13:59:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/ myappp'));
+app.get('*', function(req,res) {
+ // Replace the '/dist/<to_your_project_name>/index.html'
+ res.sendFile(path.join(__dirname + '/dist/myappp/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
